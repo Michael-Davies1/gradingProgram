ea4f3b05b8af971c6cae7beb33d24f8fe4cc0c26
Michael Davies <MD210515@reigate.ac.uk>
2021-09-20 10:56:47 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter '1' to start the program. if you wih to end the program enter '0'");
+        int runProgram = input.nextInt();
+        while (runProgram == 1)  {
+            System.out.println("please enter the total marks available");
+            int totalMarks = input.nextInt();
+            
+
+        }
+    }
+}
